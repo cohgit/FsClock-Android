6eb6ee6b4f8f43095b2da85d261f218ad20b88b6
Cesar Ogalde <cesar.ogalde@cencosudscotiabank.cl>
2026-06-08 00:49:52 -0400

    fix: import LinearLayout in FsClockView.java

--- app/src/main/java/systems/sieber/fsclock/FsClockView.java
+++ app/src/main/java/systems/sieber/fsclock/FsClockView.java
@@ -31,6 +31,7 @@ import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
